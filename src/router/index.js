55d9fcf63378daf146affbc805ae09b149fcf1b0
Hamicch <hamichtech@gmail.com>
2020-03-31 12:38:06 +0100

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+    props: true
+  },
+  {
+    path: "/destination/:slug",
+    name: "DestinationDetails",
+    props: true,
+    component: () => import(/* webpackChunkName: "DestinationDetails" */"../views/DestinationDetails"),
+    children: [
+      {
+        path: ":experienceSlug",
+        name: "experienceDetails",
+        props: true,
+        component: () => import(/* webpackChunkName: "ExperienceDetails" */ "../views/ExperienceDetails")
+      }
+    ]
+  }
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes,
+  linkExactActiveClass: "travel-app-active-link"
+});
+
+export default router;
